adaa6ccadc62bf2bb3d4dfbcf0e2d5ef2a375f3d
Matheus Yamamoto Dias <matheusyamas@gmail.com>
2025-09-12 15:36:38 -0300

    configurando os models

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Option extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['name', 'poll_id'];
+
+    public function poll(){
+        return $this->belongsTo(Poll::class);
+    }
 }
--- app/Models/Poll.php
+++ app/Models/Poll.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class Poll extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['title', 'data_inicio', 'data_termino'];
+
+    protected $casts = ['data_inicio' => 'datetime', 'data_termino' => 'datetime'];
+
+    public function option(){
+        return $this->hasMany(Option::class);
+    }
+
+    public function isRunning(): bool{
+        $now = Carbon::now();
+        if (is_null($this->data_inicio) && is_null($this->data_termino)){
+            return true;
+        }
+
+        if ($this->data_inicio && $this->data_termino){
+            return $now->between($this->data_inicio, $this->data_termino);
+        }
+        return false;
+    }
 }
